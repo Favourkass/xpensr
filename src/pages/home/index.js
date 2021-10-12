7c83f9c87795aefbc1c7bcd841e35d54626229fe
Decagon <favour.chukwuemeka@decagon.dev>
2021-10-13 00:01:51 +0100

    initial set up

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home"> 
+            <div>topfold</div>
+            <div>Expense List</div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/redux/store/index.js
@@ -0,0 +1,14 @@
+import {createStore, combineReducers} from 'redux';
+import { expenseReducer } from '../reducers/expenses';
+
+const initialState = {
+};
+const reducer = combineReducers({
+    expenses: expenseReducer
+});
+const store = createStore(
+  reducer,
+  initialState,
+);
+
+export default store;
